f8bfba2261600932bb4bcbedc94b2fa23e5ddc3e
Анастасия <citadeln@mail.ru>
2024-11-27 19:32:50 +0500

    add main

---
+++ src/debug/s21_debug.cc
@@ -0,0 +1,14 @@
+#define IFDEBUG 0
+#if IFDEBUG == 1
+
+#include <algorithm>
+#include <iostream>
+// #include "../s21_containers.h"
+
+// #include "../tests/test.h"
+
+int main() {
+  std::cout << "Well done!" << std::endl;
+}
+
+#endif
