74c92da94a240f5944c397e054f85759b16a3c14
wartorle <wartorle@student.21-school.ru>
2024-12-14 19:39:10 +0300

    add tests

--- src/set/s21_set_tests.cpp
+++ src/set/s21_set_tests.cpp
@@ -15,48 +15,40 @@ class SetInsertTest : public ::testing::Test {
 
 TEST_F(SetInsertTest, Test1_InsertIntoEmptySet) {
   s21::set<int, std::less<int>> my_set;
-  EXPECT_TRUE(my_set.insert(10));  // Успешная вставка
-  EXPECT_EQ(my_set.size(), 1);     // Размер должен быть 1
+  EXPECT_TRUE(my_set.insert(10));
+  EXPECT_EQ(my_set.size(), 1);
 }
 
-TEST_F(SetInsertTest, Test2_InsertUniqueElements) {
+TEST_F(SetInsertTest, Test2_InsertUnique) {
   s21::set<int, std::less<int>> my_set;
-  EXPECT_TRUE(my_set.insert(10));  // Успешная вставка
-  EXPECT_TRUE(my_set.insert(20));  // Успешная вставка
-  EXPECT_TRUE(my_set.insert(30));  // Успешная вставка
-  EXPECT_EQ(my_set.size(), 3);     // Размер должен быть 3
+  EXPECT_TRUE(my_set.insert(10));
+  EXPECT_TRUE(my_set.insert(20));
+  EXPECT_TRUE(my_set.insert(30));
+  EXPECT_EQ(my_set.size(), 3);
 }
 
 // Тест на вставку дублирующегося элемента
-TEST_F(SetInsertTest, Test3_InsertDuplicateElement) {
+TEST_F(SetInsertTest, Test3_InsertDuplicate) {
   s21::set<int, std::less<int>> my_set;
-  EXPECT_TRUE(my_set.insert(10));  // Успешная вставка
-  EXPECT_FALSE(my_set.insert(10));  // Неуспешная вставка (дубликат)
-  EXPECT_EQ(my_set.size(), 1);  // Размер остается 1
+  EXPECT_TRUE(my_set.insert(10));
+  EXPECT_FALSE(my_set.insert(10));  // (дубликат)
+  EXPECT_EQ(my_set.size(), 1);
 }
 
-/* // Тест с пользовательским компаратором
-struct CustomComparator {
-  bool operator()(const int& a, const int& b) const {
-    return a > b;  // Обратный порядок
-  }
-};
-
-TEST(SetInsertTest, InsertWithCustomComparator) {
-  s21::set<int, CustomComparator> my_set;
-  EXPECT_TRUE(my_set.insert(10));  // Успешная вставка
-  EXPECT_TRUE(my_set.insert(5));   // Успешная вставка
-  EXPECT_TRUE(my_set.insert(20));  // Успешная вставка
-  EXPECT_FALSE(my_set.insert(10));  // Неуспешная вставка (дубликат)
-  EXPECT_EQ(my_set.size(), 3);  // Размер должен быть 3
-}
-
-// Тест на корректную вставку с разными типами данных
-TEST(SetInsertTest, InsertDifferentTypes) {
+TEST_F(SetInsertTest, Test4_InsertDifferentTypes) {
   s21::set<std::string, std::less<std::string>> my_set;
   EXPECT_TRUE(my_set.insert("hello"));
   EXPECT_TRUE(my_set.insert("world"));
   EXPECT_FALSE(my_set.insert("hello"));  // Дубликат
   EXPECT_EQ(my_set.size(), 2);
 }
- */
+
+TEST_F(SetInsertTest, Test5_Contains_true) {
+  s21::set<int> mySet = {1, 2, 3, 4, 5};
+  EXPECT_TRUE(mySet.contains(1));
+}
+
+TEST_F(SetInsertTest, Test6_Contains_false) {
+  s21::set<int> mySet = {1, 2, 3, 4, 5};
+  EXPECT_FALSE(mySet.contains(6));
+}
