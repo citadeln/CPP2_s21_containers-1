676f3e60189eeb45887b2245af92204ec574b9da
Irbis Name <you@example.com>
2024-12-25 21:38:08 +0500

    ifndef S21_CONTAINERS_H_

--- src/s21_containers.h
+++ src/s21_containers.h
@@ -1,5 +1,11 @@
-#include "s21_list.h"
-// #include "s21_vector.h"
+#ifndef S21_CONTAINERS_H_
+#define S21_CONTAINERS_H_
+
+// #include "s21_list.h"
+#include "s21_vector.h"
 // #include "s21_stack.h"
 // #include "s21_queue.h"
 // #include "s21_map.h"
+#include "s21_array.h"
+#include "base_iter.h"
+#endif // S21_CONTAINERS_H_
